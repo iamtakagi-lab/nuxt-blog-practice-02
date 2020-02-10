fea3da7a8d5b9382fc99d04246d4a4d60483ca97
notsmatch <smatchjp@gmail.com>
2020-02-10 12:53:49 +0900

    fixed: meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,12 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'manji.fun',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      //{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      //{ hid: 'og:description', property: 'og:description',content: process.env.npm_package_description || ''  },
+      { hid: 'description', name: 'description', content: '自由気ままに書き込んでいきます' },
+      { hid: 'og:description', property: 'og:description', content: '自由気ままに書き込んでいきます'  },
       { hid: 'og:url', property: 'og:url', content: 'https://manji.fun/' },
       { hid: 'og:title', property: 'og:title', content: 'manji.fun' },
       { hid: 'og:type', property: 'og:type', content: 'website'},

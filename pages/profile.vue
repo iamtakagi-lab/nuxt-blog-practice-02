972bc4765c9a4890579bed421822431fb7478390
notsmatch <smatchjp@gmail.com>
2020-03-12 18:18:22 +0900

    fix: profie

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,13 +1,71 @@
 <template>
-  <div class="page">    
-    <h2>About me</h2>
-    <p>
-        主に、プログラミング・インフラストラクチャ・ゲーム・アニメ・音楽 など好きです。よろしくお願いいたします。
-    </p>
-    <p>
-      <a href="https://twitter.com/notsmatch">Twitter</a>
-      <a href="https://github.com/notsmatch">GitHub</a>
-    </p>
+  <div class="page">
+    <h1>About me</h1>
+    <p>主に、プログラミング・インフラストラクチャ・ゲーム・アニメ・音楽 など好きです。よろしくお願いいたします。</p>
+
+    <h2>Links</h2>
+    <ul>  
+      <li>
+        <a href="https://twitter.com/notsmatch">Twitter</a>
+      </li>
+      <li>
+        <a href="https://github.com/notsmatch">GitHub</a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCwPdVUvF4gOlJCwl9LW4hyw">YouTube</a>
+      </li>
+      <li>
+        <a href="https://www.twitch.tv/notsmatch">Twitch</a>
+      </li>
+    </ul>
+
+    <h2>Games</h2>
+    <ul>
+      <li>
+        Minecraft
+      </li>
+      <li>
+        R6S
+      </li>
+      <li>
+        PUBG
+      </li>
+      <li>
+        Fortnite
+      </li>
+      <li>
+        CS:GO
+      </li>
+      <li>
+        BF4, BF1, BFV
+      </li>
+      <li>
+        osu!
+      </li>
+       <li>
+         Rust
+       </li>
+       <li>
+         Raft
+       </li>
+    </ul>
+
+    <h2>Nintendo Games</h2>
+    <ul>
+      <li>
+        MKDS, MKwii, MK7, MK8DX
+      </li>
+      <li>
+        ポケモン第四世代 ~
+      </li>
+      <li>
+         スマブラDX, SPECIAL
+      </li>
+      <li>
+        おいでよ, とびだせ, あつまれ どうぶつの森
+      </li>
+    </ul>
+
     <p>
       Contact:
       <a href="mailto:contact.manji.fun@gmail.com">contact.manji.fun@gmail.com</a>
